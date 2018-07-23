6ae477fec6c2293c440389e94885a6c8c18dced5
Leandro Marchon <marchonleandro@gmail.com>
2018-07-23 14:49:04 -0300

    Adicionado header e navegação da página.

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
-		<title>HTML5/CSS3 Responsive Theme</title>
+		<title>HTML5/CSS3 - Website Responsivo</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width; inital-scale=1">
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 	</head>
-	<body>
-	
+	<body class="body">
+		<header class="mainheader">
+			<img src="img/logo.png" alt="Logo Topo Site">
+			<nav>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Portfólio</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+		</header>
 	</body>
 </html>
