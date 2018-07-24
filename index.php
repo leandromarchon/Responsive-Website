509f8736ab8886bcd2f423c59ca100d9c63807c5
Leandro Marchon <marchonleandro@gmail.com>
2018-07-24 15:44:13 -0300

    Adicionado valor para links.

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
 			<!-- nav -->
 			<nav>
 				<ul>
-					<li><a href="">Home</a></li>
-					<li><a href="">Sobre</a></li>
-					<li><a href="">Portfólio</a></li>
-					<li><a href="">Contato</a></li>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Sobre</a></li>
+					<li><a href="#">Portfólio</a></li>
+					<li><a href="#">Contato</a></li>
 				</ul>
 			</nav>
 		</header>
