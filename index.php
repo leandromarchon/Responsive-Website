55fec62f4aff76ce77c4b1a8c676dcfb6f00ab8e
Leandro Marchon <marchonleandro@gmail.com>
2018-07-25 21:21:05 -0300

    Corrigido parâmetros para tag <meta>.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>HTML5/CSS3 - Website Responsivo</title>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width; inital-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 	</head>
 	<body class="body">
